58a0c0c8795d5addf2320161cce692d1a63cff47
Sandhya/3105 <sandhyasurikutchi@gmail.com>
2025-03-14 21:58:43 +0530

    no

--- crop-disease-detection/vite.config.js
+++ crop-disease-detection/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DRONE-APP/', // Replace with your repository name
+  base: '/Drone-App/crop-disease-detection/', // This is the important part!
 });
