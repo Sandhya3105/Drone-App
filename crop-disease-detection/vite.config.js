bb38ad6cbf8886334058b794dd40443ff4da70c3
Sandhya/3105 <sandhyasurikutchi@gmail.com>
2025-03-14 22:31:10 +0530

    hi

--- crop-disease-detection/vite.config.js
+++ crop-disease-detection/vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for Cloudflare
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
   }
 });
