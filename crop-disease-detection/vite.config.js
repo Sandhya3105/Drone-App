cd800d8ca335736e56c82ab379a4e6caa6e2cdd5
Sandhya/3105 <sandhyasurikutchi@gmail.com>
2025-03-14 19:44:37 +0530

    2

--- crop-disease-detection/vite.config.js
+++ crop-disease-detection/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/DRONE-APP/', // Replace <your-repo-name> with your GitHub repo name
+  plugins: [react()],
+  base: '/DRONE-APP/', // Replace with your repository name
 });
-
